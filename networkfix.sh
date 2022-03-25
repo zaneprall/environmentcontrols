610069cc83be445bb80576661542b784dbce0eaf
zaneprall <99636457+zaneprall@users.noreply.github.com>
2022-03-24 21:05:13 -0600

    Update networkfix.sh

--- networkfix.sh
+++ networkfix.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-wget -q --spider http://google.com
+wget -q 
 
 if [ $? -eq 0 ]; then
-    echo "Online"
-else    echo "Offline"
+    echo "Still working. "
+else    echo "Something broke. restarting stuff. "
 sudo systemctl restart networking
 fi
 
+#script used as an easy fix for a rasberry pi zero w eventually losing networking
